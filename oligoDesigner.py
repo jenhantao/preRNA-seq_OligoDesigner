5c991d93c12ec74cf03d1e36c6dc64e322bba69f
Jenhan Tao <jenhantao@gmail.com>
2014-11-17 09:22:46 -0800

    added draft script

---
+++ oligoDesigner.py
@@ -0,0 +1,20 @@
+'''
+Designs oligos for a pre RNA-seq selection method
+'''
+
+### imports ###
+import sys
+
+def readFastaFile(fastaFilePath):
+	'''
+	Given a path to a multiline fasta file, reads the file, returning two lists - one containing the sequences, the other containing the headers
+	inputs: path to a fasta file
+	outputs: a list of the sequences, a list of the sequence headers
+	'''
+	sequences = []
+	headers = []
+
+	return sequences, headers
+
+if __name__ == "__main__":
+
